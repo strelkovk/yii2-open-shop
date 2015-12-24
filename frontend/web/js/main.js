3ee6b5acef71c3aca8f77d6b594ddd08287b3bc2
“ruslan” <sun.fiolent@yandex.ru>
2015-12-24 23:39:15 +0200

    menu main

--- frontend/web/js/main.js
+++ frontend/web/js/main.js
@@ -1,17 +1,1 @@
-/**
- * Created by Ruslan on 22.12.2015.
- */
-//$(document).ready(function() {
-//    //begin owlCarousel
-//    $("#owl-gallery").owlCarousel({
-//        autoPlay : 3000,
-//        stopOnHover : true,
-//        navigation:true,
-//        paginationSpeed : 1000,
-//        goToFirstSpeed : 2000,
-//        singleItem : true,
-//        autoHeight : true,
-//        transitionStyle:"fade"
-//    });
-//    //end owlCarousel
-//});
+
