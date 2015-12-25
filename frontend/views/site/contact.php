1857b2cdfe59057ab3c021e4f2072b0643418f75
“ruslan” <sun.fiolent@yandex.ru>
2015-12-26 01:15:55 +0200

    menu main

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -12,14 +12,15 @@ $this->title = 'Contact';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-    </p>
 
     <div class="row">
-        <div class="col-lg-5">
+        <div class="col-lg-9">
+            <div class="classic-title">
+
+                <span><h1><?= Yii::t('view','contact.info')?></h1></span>
+                <span><?= Yii::t('view','contact.title.info')?></span>
+
+            </div>
             <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
 
                 <?= $form->field($model, 'name') ?>
@@ -35,10 +36,21 @@ $this->params['breadcrumbs'][] = $this->title;
                 ]) ?>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+                    <?= Html::submitButton('Submit', ['class' => 'btn btn-system btn-large', 'name' => 'contact-button']) ?>
                 </div>
 
             <?php ActiveForm::end(); ?>
+        </div>
+        <div class="col-lg-3 text-center">
+            <div class="social-title"><span><h1><?= Yii::t('view','contact.social')?></h1></span></div>
+            <div class="s-icons">
+                <ul class="social-list">
+                    <li> <a class="facebook itl-tooltip" data-placement="bottom" title="Facebook" href="#"><i class="fa fa-facebook"></i></a> </li>
+                    <li> <a class="twitter itl-tooltip" data-placement="bottom" title="Twitter" href="#"><i class="fa fa-twitter"></i></a> </li>
+                    <li> <a class="google itl-tooltip" data-placement="bottom" title="Google Plus" href="#"><i class="fa fa-google-plus"></i></a> </li>
+                    <li> <a class="skype itl-tooltip" data-placement="bottom" title="Skype" href="#"><i class="fa fa fa-vk"></i></a> </li>
+                </ul>
+            </div>
         </div>
     </div>
 
