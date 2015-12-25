6080387a271ef124669c14243bc095107340643c
“ruslan” <sun.fiolent@yandex.ru>
2015-12-25 17:34:23 +0200

    menu main

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -42,7 +42,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <li> <a class="facebook itl-tooltip" data-placement="bottom" title="Facebook" href="#"><i class="fa fa-facebook"></i></a> </li>
                     <li> <a class="twitter itl-tooltip" data-placement="bottom" title="Twitter" href="#"><i class="fa fa-twitter"></i></a> </li>
                     <li> <a class="google itl-tooltip" data-placement="bottom" title="Google Plus" href="#"><i class="fa fa-google-plus"></i></a> </li>
-                    <li> <a class="skype itl-tooltip" data-placement="bottom" title="Skype" href="#"><i class="fa fa-skype"></i></a> </li>
+                    <li> <a class="skype itl-tooltip" data-placement="bottom" title="Skype" href="#"><i class="fa fa fa-vk"></i></a> </li>
                 </ul>
 
 
