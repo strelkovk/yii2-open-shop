98f85215ae32b2e64a931cf566ec868c316cd797
“ruslan” <sun.fiolent@yandex.ru>
2015-12-27 01:05:04 +0200

    menu main

---
+++ frontend/controllers/CatalogController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ruslan
+ * Date: 26.12.2015
+ * Time: 17:53
+ */
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Catalog controller
+ */
+class CatalogController extends Controller
+{
+
+
+    public function actionIndex()
+    {
+        $items = [
+            [
+                'img'=>'slider1',
+                'title'=>'<span>Welcome to <strong>Margo1</strong></span>',
+                'note'=>'<span>The ultimate aim of your business 1</span>',
+                'button'=>'Check Now 1',
+                'url'=>'#1',
+            ],
+            [
+                'img'=>'slider2',
+                'title'=>'<span>Welcome to <strong>Margo2</strong></span>',
+                'note'=>'<span>The ultimate aim of your business 2</span>',
+                'button'=>'Check Now 2',
+                'url'=>'#2',
+            ],
+            [
+                'img'=>'slider3',
+                'title'=>'<span>Welcome to <strong>Margo3</strong></span>',
+                'note'=>'<span>The ultimate aim of your business 3</span>',
+                'button'=>'Check Now 3',
+                'url'=>'#3',
+            ],
+
+        ];
+        return $this->render('index', ['items' => $items] );
+
+    }
+
+
+}
---
+++ frontend/controllers/ServiceController.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ruslan
+ * Date: 27.12.2015
+ * Time: 1:00
+ */
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Service controller
+ */
+class ServiceController extends Controller
+{
+
+
+    public function actionIndex()
+    {
+        $items = [
+            [
+                'img'=>'slider1',
+                'title'=>'<span>Welcome to <strong>Margo1</strong></span>',
+                'note'=>'<span>The ultimate aim of your business 1</span>',
+                'button'=>'Check Now 1',
+                'url'=>'#1',
+            ],
+            [
+                'img'=>'slider2',
+                'title'=>'<span>Welcome to <strong>Margo2</strong></span>',
+                'note'=>'<span>The ultimate aim of your business 2</span>',
+                'button'=>'Check Now 2',
+                'url'=>'#2',
+            ],
+            [
+                'img'=>'slider3',
+                'title'=>'<span>Welcome to <strong>Margo3</strong></span>',
+                'note'=>'<span>The ultimate aim of your business 3</span>',
+                'button'=>'Check Now 3',
+                'url'=>'#3',
+            ],
+
+        ];
+        return $this->render('index', ['items' => $items] );
+
+    }
+
+
+}
